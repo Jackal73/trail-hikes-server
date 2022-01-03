11d7c0c33fab3508108a74bc473beb4019a7e141
Shawn Kebel <fxrender@outlook.com>
2022-01-02 21:12:05 -0600

    :goal_net: Add 'error handling' for routes

--- app/index.js
+++ app/index.js
@@ -7,4 +7,13 @@ const app = express();
 
 app.use("/api/hikes", hikesRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
 app.listen(5000);
--- routes/hikes-routes.js
+++ routes/hikes-routes.js
@@ -16,15 +16,31 @@ const DUMMY_HIKES = [
   },
 ];
 
-router.get("/:pid", (req, res) => {
+router.get("/:pid", (req, res, next) => {
   const hikeId = req.params.pid; // { pid: 'p1' }
+
   const hike = DUMMY_HIKES.find((p) => p.id === hikeId);
+
+  if (!hike) {
+    const error = new Error("Could not find a hike for the specified pid.");
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ hike }); // { hike } => { hike: hike }
 });
 
-router.get("/user/:uid", (req, res) => {
+router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
+
   const hike = DUMMY_HIKES.find((p) => p.creator === userId);
+
+  if (!hike) {
+    const error = new Error("Could not find a hike for the specified user id.");
+    error.code = 404;
+    return next(error);
+  }
+
   res.json({ hike });
 });
 export default router;
