5406e2fc10618095971a68f69050db267bbe61ab
Shawn Kebel <fxrender@outlook.com>
2022-01-03 16:15:43 -0600

    :recycle: Refactor for `export default`

--- controllers/hikes-controller.js
+++ controllers/hikes-controller.js
@@ -1,5 +1,4 @@
 import { v4 as uuidv4 } from "uuid";
-
 import HttpError from "../models/http-error.js";
 
 const DUMMY_HIKES = [
@@ -16,52 +15,53 @@ const DUMMY_HIKES = [
   },
 ];
 
-const getHikeById = (req, res, next) => {
-  const hikeId = req.params.pid; // { pid: 'p1' }
+export default {
+  getHikeById(req, res, next) {
+    const hikeId = req.params.pid; // { pid: 'p1' }
 
-  const hike = DUMMY_HIKES.find((p) => p.id === hikeId);
+    const hike = DUMMY_HIKES.find((p) => p.id === hikeId);
 
-  if (!hike) {
-    return next(
-      new HttpError("No hike was found for the specified hike id.", 404)
-    );
-  }
+    if (!hike) {
+      return next(
+        new HttpError("No hike was found for the specified hike id.", 404)
+      );
+    }
 
-  res.json({ hike }); // { hike } => { hike: hike }
+    res.json({ hike }); // { hike } => { hike: hike }
+  },
+
+  getHikeByUserId(req, res, next) {
+    const userId = req.params.uid;
+
+    const hike = DUMMY_HIKES.find((p) => p.creator === userId);
+
+    if (!hike) {
+      return next(
+        new HttpError(
+          "No hike(s) could be found for the specified user id.",
+          404
+        )
+      );
+    }
+
+    res.json({ hike });
+  },
+
+  createHike(req, res, next) {
+    const { title, description, coordinates, address, creator } = req.body;
+    // const title = req.body.title, ...;
+
+    const createdHike = {
+      id: uuidv4(),
+      title,
+      description,
+      location: coordinates,
+      address,
+      creator,
+    };
+
+    DUMMY_HIKES.push(createdHike); // unshift (createdHike) - first element
+
+    res.status(201).json({ hike: createdHike });
+  },
 };
-
-const getHikeByUserId = (req, res, next) => {
-  const userId = req.params.uid;
-
-  const hike = DUMMY_HIKES.find((p) => p.creator === userId);
-
-  if (!hike) {
-    return next(
-      new HttpError("No hike(s) could be found for the specified user id.", 404)
-    );
-  }
-
-  res.json({ hike });
-};
-
-const createHike = (req, res, next) => {
-  const { title, description, coordinates, address, creator } = req.body;
-  // const title = req.body.title, ...;
-
-  const createdHike = {
-    id: uuidv4(),
-    title,
-    description,
-    location: coordinates,
-    address,
-    creator,
-  };
-
-  DUMMY_HIKES.push(createdHike); // unshift (createdHike) - first element
-
-  res.status(201).json({ place: createdHike });
-};
-
-exports.getHikeById = getHikeById;
-exports.getHikeByUserId = getHikeByUserId;
-exports.createHike = createHike;
