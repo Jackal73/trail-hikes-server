ea51e2a73317c408d764977f73a0233b850aad19
Shawn Kebel <fxrender@outlook.com>
2022-01-10 15:26:41 -0600

    :sparkles: GET hike by `hikeId`

--- controllers/hikes-controller.js
+++ controllers/hikes-controller.js
@@ -18,18 +18,29 @@ let DUMMY_HIKES = [
 ];
 
 export default {
-  getHikeById(req, res, next) {
+  async getHikeById(req, res, next) {
     const hikeId = req.params.pid; // { pid: 'p1' }
 
-    const hike = DUMMY_HIKES.find((p) => p.id === hikeId);
-
-    if (!hike) {
-      return next(
-        new HttpError("No hike was found for the specified hike id.", 404)
+    let hike;
+    try {
+      hike = await Hike.findById(hikeId);
+    } catch (err) {
+      const error = new HttpError(
+        "Something went wrong, could not find a hike.",
+        500
       );
+      return next(error);
     }
 
-    res.json({ hike }); // { hike } => { hike: hike }
+    if (!hike) {
+      const error = new HttpError(
+        "Could not find a hike for the provided Id.",
+        404
+      );
+      return next(error);
+    }
+
+    res.json({ hike: hike.toObject({ getters: true }) }); // => { hike } => { hike: hike }
   },
 
   getHikesByUserId(req, res, next) {
