f9a3f3976d86397cba11de97480334ae888659d1
Shawn Kebel <fxrender@outlook.com>
2022-01-10 21:42:33 -0600

    :pencil2: Change 'places' to 'hikes'

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
-  places: { type: String, required: true },
+  hikes: { type: String, required: true },
 });
 
 userSchema.plugin(uniqueValidator);
