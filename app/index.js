375af169c09444b2320d0c7ebd65d24fda4ad3ee
Shawn Kebel <fxrender@outlook.com>
2022-01-12 16:06:56 -0600

    :necktie: Add middleware for `CORS` errors

--- app/index.js
+++ app/index.js
@@ -9,6 +9,16 @@ const app = express();
 // Middleware that allows express to read incoming json requests
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/hikes", hikesRoutes);
 app.use("/api/users", usersRoutes);
 
