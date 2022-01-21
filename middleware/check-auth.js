fd85852ecacc5e51a789b34f25e96e10ef2fc367
Shawn Kebel <fxrender@outlook.com>
2022-01-21 09:38:08 -0600

    :bug: Fix 'OPTIONS' issue : `check-auth`

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,9 +1,14 @@
 import jwt from "jsonwebtoken";
 import HttpError from "../models/http-error.js";
 
-module.exports = (req, res, next) => {
+export default (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
   try {
     const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+
     if (!token) {
       throw new Error("Authentication failed!");
     }
