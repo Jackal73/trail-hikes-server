fd912b5fec874a97c68423347eed881f9d74164f
Shawn Kebel <fxrender@outlook.com>
2022-01-21 00:35:10 -0600

    :boom::sparkles: Generate a Json Web Token `JWT`

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,6 @@
-import { validationResult } from "express-validator";
 import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import HttpError from "../models/http-error.js";
 import User from "../models/user.js";
 
@@ -56,7 +57,7 @@ export default {
         "Could not create user, please try again.",
         500
       );
-      next(error);
+      return next(error);
     }
 
     const createdUser = new User({
@@ -74,7 +75,21 @@ export default {
       return next(error);
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token;
+    try {
+      token = jwt.sign(
+        { userId: createdUser.id, email: createdUser.email },
+        "supersecret_dont_share",
+        { expiresIn: "1h" }
+      );
+    } catch (err) {
+      const error = new HttpError("Signup failed, please try again.", 500);
+      return next(error);
+    }
+
+    res
+      .status(201)
+      .json({ userId: createdUser.id, email: createdUser.email, token });
   },
 
   async login(req, res, next) {
@@ -104,7 +119,7 @@ export default {
         "Could not log in, please check credentials and try again.",
         500
       );
-      return next;
+      return next(error);
     }
 
     if (!isValidPassword) {
@@ -115,9 +130,22 @@ export default {
       return next(error);
     }
 
+    let token;
+    try {
+      token = jwt.sign(
+        { userId: existingUser.id, email: existingUser.email },
+        "supersecret_dont_share",
+        { expiresIn: "1h" }
+      );
+    } catch (err) {
+      const error = new HttpError("Login failed, please try again.", 500);
+      return next(error);
+    }
+
     res.json({
-      message: "Logged in!",
-      user: existingUser.toObject({ getters: true }),
+      userId: existingUser.id,
+      email: existingUser.email,
+      token,
     });
   },
 };
